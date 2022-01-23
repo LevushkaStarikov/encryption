71e7dfe312e8ca22f8f6978003f94050068bbb66
LevushkaStarikov <lev-starikov@mail.ru>
2022-01-23 20:46:59 +0300

    Initial commit

---
+++ tasks.hpp
@@ -0,0 +1,17 @@
+//
+// Created by Lev Starikov on 23.01.2022.
+//
+
+#ifndef ENCRYPTION_TASKS_HPP
+#define ENCRYPTION_TASKS_HPP
+
+#endif //ENCRYPTION_TASKS_HPP
+
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+void dec(const string &);
+
+void enc(const string &);
